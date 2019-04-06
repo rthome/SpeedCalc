d57d2e37c7741ca69b8bdb182fcb7d214516ee5a
Raffael Thome <mail@rmt.name>
2019-04-06 15:40:16 +0200

    Add ToString, Parse and TryParse method declarations to BigInteger

--- SpeedCalc.Numeric/BigInteger.cs
+++ SpeedCalc.Numeric/BigInteger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace SpeedCalc.Numeric
 {
@@ -8,7 +9,7 @@ namespace SpeedCalc.Numeric
     public readonly struct BigInteger : IComparable<BigInteger>, IEquatable<BigInteger>
     {
         readonly int sign;
-        readonly Memory<uint> bits;
+        readonly ReadOnlyMemory<uint> bits;
 
         #region Static Properties
 
@@ -26,6 +27,84 @@ namespace SpeedCalc.Numeric
         {
             throw new NotImplementedException();
         }
+
+        #endregion
+
+        #region Static Parse Methods
+
+        /// <summary>
+        /// Parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <returns>A new big integer</returns>
+        public static BigInteger Parse(string value) => Parse(value, NumberStyles.Integer, NumberFormatInfo.CurrentInfo);
+
+        /// <summary>
+        /// Parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="style">Number styles to permit</param>
+        /// <returns>A new big integer</returns>
+        public static BigInteger Parse(string value, NumberStyles style) => Parse(value, style, NumberFormatInfo.CurrentInfo);
+
+        /// <summary>
+        /// Parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="provider">Number format provider to use for culture-dependent formatting</param>
+        /// <returns>A new big integer</returns>
+        public static BigInteger Parse(string value, IFormatProvider provider) => Parse(value, NumberStyles.Integer, provider);
+
+        /// <summary>
+        /// Parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="style">Number styles to permit</param>
+        /// <param name="provider">Number format provider to use for culture-dependent formatting</param>
+        /// <returns>A new big integer</returns>
+        public static BigInteger Parse(string value, NumberStyles style, IFormatProvider provider)
+        {
+            if (TryParse(value, style, provider, out var result))
+                return result;
+            else
+                throw new FormatException("Unable to parse big integer from given string");
+        }
+
+        /// <summary>
+        /// Try to parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="result">If successful, contains the parsed big integer</param>
+        /// <returns>True if a big integer could be parsed; otherwise false</returns>
+        public static bool TryParse(string value, out BigInteger result) => TryParse(value, NumberStyles.Integer, NumberFormatInfo.CurrentInfo, out result);
+
+        /// <summary>
+        /// Try to parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="style">Number styles to permit</param>
+        /// <param name="result">If successful, contains the parsed big integer</param>
+        /// <returns>True if a big integer could be parsed; otherwise false</returns>
+        public static bool TryParse(string value, NumberStyles style, out BigInteger result) => TryParse(value, style, NumberFormatInfo.CurrentInfo, out result);
+
+        /// <summary>
+        /// Try to parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="provider">Number format provider to use for culture-dependent formatting</param>
+        /// <param name="result">If successful, contains the parsed big integer</param>
+        /// <returns>True if a big integer could be parsed; otherwise false</returns>
+        public static bool TryParse(string value, IFormatProvider provider, out BigInteger result) => TryParse(value, NumberStyles.Integer, provider, out result);
+
+        /// <summary>
+        /// Try to parse a big integer from a string
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="style">Number styles to permit</param>
+        /// <param name="provider">Number format provider to use for culture-dependent formatting</param>
+        /// <param name="result">If successful, contains the parsed big integer</param>
+        /// <returns>True if a big integer could be parsed; otherwise false</returns>
+        public static bool TryParse(string value, NumberStyles style, IFormatProvider provider, out BigInteger result) => BigIntegerUtils.TryParse(value, style, NumberFormatInfo.GetInstance(provider), out result);
 
         #endregion
 
@@ -61,12 +140,33 @@ namespace SpeedCalc.Numeric
 
         #region ToString
 
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+        /// <summary>
+        /// Format the big integer as a string
+        /// </summary>
+        /// <returns>A string representation of the big integer</returns>
+        public override string ToString() => ToString(null, NumberFormatInfo.CurrentInfo);
 
-        // TODO: Add overloads with number styles, format providers, etc
+        /// <summary>
+        /// Format the big integer as a string
+        /// </summary>
+        /// <param name="provider">The format provider to use</param>
+        /// <returns>A string representation of the big integer</returns>
+        public string ToString(IFormatProvider provider) => ToString(null, provider);
+
+        /// <summary>
+        /// Format the big integer as a string
+        /// </summary>
+        /// <param name="format">The format string to use</param>
+        /// <returns>A string representation of the big integer</returns>
+        public string ToString(string format) => ToString(format, NumberFormatInfo.CurrentInfo);
+
+        /// <summary>
+        /// Format the big integer as a string
+        /// </summary>
+        /// <param name="format">The format string to use</param>
+        /// <param name="provider">The format provider to use</param>
+        /// <returns>A string representation of the big integer</returns>
+        public string ToString(string format, IFormatProvider provider) => BigIntegerUtils.ToString(this, format, NumberFormatInfo.GetInstance(provider));
 
         #endregion
 
---
+++ SpeedCalc.Numeric/BigIntegerUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace SpeedCalc.Numeric
+{
+    static class BigIntegerUtils
+    {
+        public static string ToString(in BigInteger value, string format, NumberFormatInfo formatInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse(string value, NumberStyles style, NumberFormatInfo formatInfo, out BigInteger result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
