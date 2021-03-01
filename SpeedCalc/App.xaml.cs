5b010a57a78f3ea1a3686a0d95e8aada3ef9c6b8
Raffael Thome <mail@rmt.name>
2021-03-02 00:31:55 +0100

    Set up empty window with RxUI+DI

--- SpeedCalc/App.xaml.cs
+++ SpeedCalc/App.xaml.cs
@@ -1,17 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+
+using ReactiveUI;
+
+using SpeedCalc.ViewModels;
+using SpeedCalc.Views;
+
+using Splat;
+using Splat.Autofac;
+
+using System;
 using System.Windows;
 
 namespace SpeedCalc
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        void ConfigureIoC()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(GetType().Assembly)
+                .PublicOnly()
+                .AsClosedTypesOf(typeof(IViewFor<>))
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterAssemblyTypes(GetType().Assembly)
+                .PublicOnly()
+                .Where(type => type.Name.EndsWith("ViewModel", StringComparison.InvariantCultureIgnoreCase))
+                .AsSelf();
+
+            // Workaround for https://github.com/reactiveui/splat/issues/645
+            var resolver = builder.UseAutofacDependencyResolver();
+            resolver.InitializeSplat();
+            resolver.InitializeReactiveUI();
+            builder.RegisterInstance(resolver);
+
+            var container = builder.Build();
+            resolver.SetLifetimeScope(container);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ConfigureIoC();
+
+            var mainView = (CalculatorView)Locator.Current.GetService<IViewFor<CalculatorViewModel>>();
+            mainView.Show();
+        }
     }
 }
---
+++ SpeedCalc/Views/CalculatorView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+
+using SpeedCalc.ViewModels;
+
+namespace SpeedCalc.Views
+{
+    public partial class CalculatorView : ReactiveWindow<CalculatorViewModel>
+    {
+        public CalculatorView(CalculatorViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+
+            this.WhenActivated(disposables =>
+            {
+
+            });
+        }
+    }
+}
