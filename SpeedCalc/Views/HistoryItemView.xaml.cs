4336bc00dac1b6fb482b7a23ab30ef1d088c2a76
Raffael Thome <mail@rmt.name>
2021-03-04 22:15:13 +0100

    Add a few more GUI scraps

---
+++ SpeedCalc/Views/HistoryItemView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+
+using SpeedCalc.ViewModels;
+
+namespace SpeedCalc.Views
+{
+    public partial class HistoryItemView : ReactiveUserControl<HistoryItemViewModel>
+    {
+        public HistoryItemView(HistoryItemViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+
+            this.WhenActivated(disposables =>
+            {
+
+            });
+        }
+    }
+}
